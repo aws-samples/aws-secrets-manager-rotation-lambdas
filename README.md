5487db553bc9970a8bcfbd5d8f81498c395c9a7a
Jerico Pena <jericop@gmail.com>
2023-11-05 19:13:59 -0500

    Build images for all rotation functions

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Secrets Manager provides rotation function templates for several types of credentials. To use the templates, see https://docs.aws.amazon.com/secretsmanager/latest/userguide/reference_available-rotation-templates.html.
 
+* This fork builds and publishes multi-arch container images as configured in `images.json`.
+* The repo(s) that the images are published to are specified in the `.push_to_repos` key.
+* Each image builds the folder and then pushes to the repo using the specified tag.
+
 ## License Summary
 
 This sample code is made available under a modified MIT license. See the LICENSE file.
--- build-images.sh
+++ build-images.sh
@@ -10,13 +10,15 @@ for row in $(cat images.json | jq -r '.folders[] | @base64'); do
   }
 
   folder=$(_jq '.folder')
-  packages=$(_jq '.required_packages')
+  system_packages=$(_jq '.install_system_packages') # returns 'null' if key is not present
+  python_packages=$(_jq '.python_packages')         # returns 'null' if key is not present
   tag=$(_jq '.tag')
   
   cp Dockerfile $folder
   
   docker buildx build \
-    --build-arg "packages=$packages" \
+    --build-arg "system_packages=$system_packages" \
+    --build-arg "python_packages=$python_packages" \
     --platform linux/amd64,linux/arm64 \
     --tag "$registry_repo:$tag" \
     --push  $folder 
