04da3a54c4c4c0ea8b9fc94df3a64eeb68fb12cf
Jerico Pena <jericop@gmail.com>
2023-11-06 21:26:16 -0500

    Add buildx cache

--- build-images.sh
+++ build-images.sh
@@ -5,13 +5,6 @@ set -x
 
 registry_repo="${1:-ignored-not-pushed}"
 
-docker_push_arg="--push"
-
-# Remove push flag if registry_repo was not specified
-if [[ "$registry_repo" == "ignored-not-pushed" ]] ; then
-  docker_push_arg=""
-fi
-
 for row in $(cat images.json | jq -r '.folders[] | @base64'); do
   _jq() {
     echo ${row} | base64 --decode | jq -r ${1}
@@ -23,11 +16,23 @@ for row in $(cat images.json | jq -r '.folders[] | @base64'); do
   tag=$(_jq '.tag')
   
   cp Dockerfile $folder
+
+  registry_repo_cache="$registry_repo-buildx-cache"
+
+  docker_push_arg="--push"
+  docker_cache_to_arg="--cache-to type=registry,ref=$registry_repo_cache,mode=max"
+  docker_cache_from_arg="--cache-from type=registry,ref=$registry_repo_cache"
+  docker_cache_args="$docker_cache_to_arg $docker_cache_from_arg"
+
+  if [[ "$registry_repo" == "ignored-not-pushed" ]] ; then
+    docker_push_arg=""
+    docker_cache_args="$docker_cache_from_arg"
+  fi
   
   docker build \
     --build-arg "system_packages=$system_packages" \
     --build-arg "python_packages=$python_packages" \
-    --tag "$registry_repo:$tag" $docker_push_arg \
+    --tag "$registry_repo:$tag" $docker_push_arg $docker_cache_args \
     $folder
   
   rm $folder/Dockerfile
